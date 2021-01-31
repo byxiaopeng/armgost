aa2c6cbc5b25a877ef7232a7fd121e8d21248594
小朋 <2810580330@qq.com>
2021-01-31 19:49:20 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ ! -e 'node /elecV2P/Shell/scripts/jd_crazy_joy_coin.js' ]; then
+if [ ! -e '/elecV2P/Shell/scripts/jd_crazy_joy_coin.js' ]; then
     cp -f /tmp/Shell/scripts/* /elecV2P/Shell/scripts
 fi
 node /elecV2P/Shell/scripts/jd_crazy_joy_coin.js >/dev/null 2>&1 &
