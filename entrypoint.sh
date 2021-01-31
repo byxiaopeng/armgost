c1cbf5619dcb342f5bfd7daa88618666f7a865e0
小朋 <2810580330@qq.com>
2021-01-31 19:48:49 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+if [ ! -e 'node /elecV2P/Shell/scripts/jd_crazy_joy_coin.js' ]; then
+    cp -f /tmp/Shell/scripts/* /elecV2P/Shell/scripts
+fi
 node /elecV2P/Shell/scripts/jd_crazy_joy_coin.js >/dev/null 2>&1 &
 echo "启动jd_crazy_joy_coin挂机完成"
 cd /elecV2P && yarn start
