de45723671fa5d109030df06f61f423f8dabd575
小朋 <2810580330@qq.com>
2021-02-04 13:13:20 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,9 +13,15 @@ if [ ! -e '/elecV2P/script/Lists/useragent.list' ]; then
     cp -r /tmp/Lists /elecV2P/script
     echo "拷贝Lists代码"
 fi
+
+if [[ ${ENABLE_HANGUP} == true ]]; then
+    node /elecV2P/script/Shell/scripts/jd_crazy_joy_coin.js >/dev/null 2>&1 &
+    echo -e "启动joy挂机程序"
+  fi
+elif [[ ${ENABLE_HANGUP} == false ]]; then
+  echo -e "设置不挂机joy，跳过"
+fi
 #node /elecV2P/script/Shell/scripts/jd_crazy_joy_coin.js >/dev/null 2>&1 &
 #echo "启动jd_crazy_joy_coin挂机完成"
-/usr/sbin/sshd
-echo "启动SSH成功"
 cd /elecV2P && pm2 start index.js
 tail -f /dev/null
