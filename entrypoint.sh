31890e0535460b8e18d1f6bd9503a6245732f8b1
小朋 <2810580330@qq.com>
2021-02-26 00:30:15 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,7 +19,16 @@ if [ ! -e '/elecV2P/script/Shell/gx.sh' ]; then
     mv /gx.sh /elecV2P/script/Shell/gx.sh
     echo "拷贝更新代码"
 fi
-echo "首次启动更新仓库代码和挂机判断"
-/elecV2P/script/Shell/gx.sh
+echo "开始判断是否挂机joy"
+if [[ ${ENABLE_HANGUP} == true ]]; then
+    jdpid=$(ps -ef | grep "jd_crazy" | awk '{print $1}')
+    echo "joy进程pid=$jdpid"
+    kill $jdpid
+    echo "配置jd_crazy_joy_coin重启完成"
+    node /elecV2P/script/Shell/scripts/jd_crazy_joy_coin.js >/dev/null 2>&1 &
+    echo "启动jd_crazy_joy_coin挂机完成"
+elif [[ ${ENABLE_HANGUP} == false ]]; then
+  echo -e "没有设置变量为true  不执行joy挂机"
+fi
 cd /elecV2P && pm2 start index.js --name elecV2P
 tail -f /dev/null
