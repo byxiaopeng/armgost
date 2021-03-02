9b00fc9630ac1a0f2a919736e268998864a439f3
小朋 <2810580330@qq.com>
2021-03-02 18:55:23 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ ! -e '/elecV2P/script/Shell/scripts/jd_crazy_joy_coin.js' ]; then
+if [ ! -e '/elecV2P/script/Shell/scripts/jdCookie.js' ]; then
     cp -r /tmp/Shell/scripts /elecV2P/script/Shell
     echo "拷贝仓库代码"
 fi
@@ -13,17 +13,5 @@ if [ ! -e '/elecV2P/script/Lists/task.list' ]; then
     wget -P /elecV2P/script/Lists -q https://www.armn1.ml/jiaoben/task.list
     echo "拷贝Lists代码"
 fi
-
-echo "开始判断是否挂机joy"
-if [[ ${ENABLE_HANGUP} == true ]]; then
-    jdpid=$(ps -ef | grep "jd_crazy" | awk '{print $1}')
-    echo "joy进程pid=$jdpid"
-    kill $jdpid
-    echo "配置jd_crazy_joy_coin重启完成"
-    node /elecV2P/script/Shell/scripts/jd_crazy_joy_coin.js >/dev/null 2>&1 &
-    echo "启动jd_crazy_joy_coin挂机完成"
-elif [[ ${ENABLE_HANGUP} == false ]]; then
-  echo -e "没有设置变量为true  不执行joy挂机"
-fi
-cd /elecV2P && pm2 start index.js --name elecV2P
+elecV2P && pm2 start index.js --name elecV2P
 tail -f /dev/null
