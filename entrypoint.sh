4cec7a43886a57515d6997151dc6a2eabeb1afa7
小朋 <2810580330@qq.com>
2021-03-18 07:50:57 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-case $TYPE in
-"git")
-  rm -r /usr/local/app/script/Store/cookieKEY
-  rm -r /usr/local/app/script/Store/newone
-  git clone ${GIT_URL} /usr/local/app/script/Store
-  echo "usr/local/app/script/Store目录"
-  break
-  ;;
-esac
 if [ ! -e '/usr/local/app/script/Shell/scripts/jdCookie.js' ]; then
     cp -r /tmp/Shell/scripts /usr/local/app/script/Shell
     cd /usr/local/app/script/Shell/scripts && npm install
