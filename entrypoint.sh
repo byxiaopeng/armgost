a2aeb4b744e25c23ce14f8a4db4c024d73dc9dd1
小朋 <2810580330@qq.com>
2021-04-17 19:51:45 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ if [ ! -e '/usr/local/app/script/Shell/test.py' ]; then
     echo "拷贝Shell代码"
 fi
 if [ ! -e '/usr/local/app/script/JSFile/jd/jdCookie.js' ]; then
-    cp -r /tmp/Shell/scripts /usr/local/app/script/jd
+    cp -r /tmp/jd/scripts /usr/local/app/script/jd
 fi
 if [ ! -e '/usr/local/app/script/JSFile/0body.js' ]; then
     cp -r /tmp/JSFile /usr/local/app/script
